6af390c78556f22b7b1feb8ec25cdb9665d8bc72
Denis Kutnár <deniskutnar@Deniss-MacBook-Pro.local>
2022-07-15 13:57:13 +0200

    “first”

---
+++ crop.py
@@ -0,0 +1,21 @@
+import os
+import SimpleITK as sitk
+import numpy as np
+from glob import glob
+
+def read_image(path):
+    img = sitk.ReadImage(path)
+    img_as_numpy = sitk.GetArrayFromImage(img)
+    return img_as_numpy
+
+
+CT = read_image('/home/abe/samba_share/katrins_data/18103/ct_from_-218_to_192.nii.gz')
+PET = read_image('/home/abe/samba_share/katrins_data/18103/pet_from_-782_192.nii.gz')
+print(CT.shape)
+print(PET.shape)
+
+
+
+dirs = glob('/home/abe/samba_share/katrins_data/*/*ct_from*')
+print(len(dirs))
+
