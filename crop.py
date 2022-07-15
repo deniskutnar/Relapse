85493ceb9a7187e6b04807e042e895acea307f5c
Denis Kutnár <deniskutnar@Deniss-MacBook-Pro.local>
2022-07-15 14:07:26 +0200

    “first”

--- crop.py
+++ crop.py
@@ -3,6 +3,11 @@ import SimpleITK as sitk
 import numpy as np
 from glob import glob
 
+#dirs = glob('/home/abe/samba_share/katrins_data/*/*ct_from*')
+#print(len(dirs))
+
+im = sitk.ReadImage('/home/abe/samba_share/katrins_data/18103/ct_from_-218_to_192.nii.gz')
+
 def read_image(path):
     img = sitk.ReadImage(path)
     img_as_numpy = sitk.GetArrayFromImage(img)
@@ -14,8 +19,13 @@ PET = read_image('/home/abe/samba_share/katrins_data/18103/pet_from_-782_to_192.
 print(CT.shape)
 print(PET.shape)
 
+PET_cropped = CT[:,:, 282:206]
+PET_itk = sitk.GetImageFromArray(PET_cropped)
+PET_itk.CopyInformation(im)
+
+sitk.WriteImage(PET_itk, '/home/abe/samba_share/katrins_data/18103/PET_cropped.nii.gz')
+print("done")
 
 
-dirs = glob('/home/abe/samba_share/katrins_data/*/*ct_from*')
-print(len(dirs))
+
 
