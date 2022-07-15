4f7a7d68179e079bde4ad3395b9c5af7394590ce
Denis Kutnár <deniskutnar@Deniss-MacBook-Pro.local>
2022-07-15 14:19:16 +0200

    “first”

--- crop.py
+++ crop.py
@@ -3,19 +3,21 @@ import SimpleITK as sitk
 import numpy as np
 from glob import glob
 
-#dirs = glob('/home/abe/samba_share/katrins_data/*/*ct_from*')
+#dirs = glob('/home/denis/samba_share/katrins_data/*/*ct_from*')
 #print(len(dirs))
 
-im = sitk.ReadImage('/home/abe/samba_share/katrins_data/18103/ct_from_-218_to_192.nii.gz')
+im = sitk.ReadImage('/home/denis/samba_share/katrins_data/18103/ct_from_-218_to_192.nii.gz')
 
 def read_image(path):
     img = sitk.ReadImage(path)
     img_as_numpy = sitk.GetArrayFromImage(img)
     return img_as_numpy
 
+/home/denis/samba_share/katrins_data
 
-CT = read_image('/home/abe/samba_share/katrins_data/18103/ct_from_-218_to_192.nii.gz')
-PET = read_image('/home/abe/samba_share/katrins_data/18103/pet_from_-782_to_192.nii.gz')
+
+CT = read_image('/home/denis/samba_share/katrins_data/18103/ct_from_-218_to_192.nii.gz')
+PET = read_image('/home/denis/samba_share/katrins_data/18103/pet_from_-782_to_192.nii.gz')
 print(CT.shape)
 print("new pet: ", PET.shape)
 
@@ -24,7 +26,7 @@ print(PET_cropped.shape)
 PET_itk = sitk.GetImageFromArray(PET_cropped)
 #PET_itk.CopyInformation(im)
 
-sitk.WriteImage(PET_itk, '/home/abe/samba_share/katrins_data/18103/PET_cropped.nii.gz')
+sitk.WriteImage(PET_itk, '/home/denis/samba_share/katrins_data/18103/PET_cropped.nii.gz')
 print("done")
 
 
