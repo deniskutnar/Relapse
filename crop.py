f457538b6b3d4c0ac94dc7b2e20b67d79004e067
Denis Kutnár <deniskutnar@Deniss-MacBook-Pro.local>
2022-07-20 10:23:17 +0200

    “first”

--- crop.py
+++ crop.py
@@ -3,6 +3,73 @@ import SimpleITK as sitk
 import numpy as np
 from glob import glob
 import shutil
+from pydicom import dcmread
+import matplotlib.pyplot as plt
+
+
+
+def resize_image_itk(ori_img, target_img, resamplemethod=sitk.sitkNearestNeighbor):
+    """
+    use itk Method to convert the original image resample To be consistent with the target image
+    :param ori_img: Original alignment required itk image
+    :param target_img: Target to align itk image
+    :param resamplemethod: itk interpolation method : sitk.sitkLinear-linear  sitk.sitkNearestNeighbor-Nearest neighbor
+    :return:img_res_itk: Resampling okay itk image
+    """
+    target_Size = target_img.GetSize()      # Target image size [x,y,z]
+    target_Spacing = target_img.GetSpacing()   # Voxel block size of the target [x,y,z]
+    target_origin = target_img.GetOrigin()      # Starting point of target [x,y,z]
+    target_direction = target_img.GetDirection()  # Target direction [crown, sagittal, transverse] = [z,y,x]
+
+    # The method of itk is resample
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(ori_img)  # Target image to resample
+    # Set the information of the target image
+    resampler.SetSize(target_Size)		# Target image size
+    resampler.SetOutputOrigin(target_origin)
+    resampler.SetOutputDirection(target_direction)
+    resampler.SetOutputSpacing(target_Spacing)
+    # Set different dype according to the need to resample the image
+    if resamplemethod == sitk.sitkNearestNeighbor:
+        resampler.SetOutputPixelType(sitk.sitkUInt16)   # Nearest neighbor interpolation is used for mask, and uint16 is saved
+    else:
+        resampler.SetOutputPixelType(sitk.sitkFloat32)  # Linear interpolation is used for PET/CT/MRI and the like, and float32 is saved
+    resampler.SetTransform(sitk.Transform(3, sitk.sitkIdentity))    
+    resampler.SetInterpolator(resamplemethod)
+    itk_img_resampled = resampler.Execute(ori_img)  # Get the resampled image
+    return itk_img_resampled
+
+
+
+ct_dir = "/home/denis/samba_share/katrins_data/7229/Cropped/CT.nii.gz"
+pet_dir = "/home/denis/samba_share/katrins_data/7229/Cropped/PET.nii.gz"
+mask_dir = "/home/denis/samba_share/katrins_data/7229/Cropped/GTV.nii.gz"
+
+## resample PET --> CT
+t_img = sitk.ReadImage(ct_dir)
+o_img = sitk.ReadImage(pet_dir)
+reg_pet = resize_image_itk(o_img, t_img, sitk.sitkLinear)
+
+ct = read_image(ct_dir)
+pet = sitk.GetArrayFromImage(reg_pet).astype('float32')
+mask = read_image(mask_dir)
+
+print(ct.shape)
+print(pet.shape)
+print(mask.shape)
+
+f, ax = plt.subplots(2, 2, figsize=(10, 10))
+ax[0][0].imshow(ct.max(0),  cmap = 'gray')
+ax[0][0].imshow(pet.max(0),  cmap = 'Reds', alpha=0.3)
+ax[0][1].imshow(ct.max(0), cmap = 'gray')
+ax[0][1].imshow(mask.max(0), cmap = 'Reds', alpha=0.3)
+
+ax[1][0].imshow(pet.max(0),  cmap = 'gray')
+ax[1][1].imshow(pet.max(0), cmap = 'gray')
+ax[1][1].imshow(mask.max(0), cmap = 'Reds', alpha=0.3)
+
+f.savefig("plot.png")
+
 
 def read_image(path):
     img = sitk.ReadImage(path)
@@ -60,7 +127,7 @@ OneS_dirs = ["/home/denis/samba_share/katrins_data/8935", # missing relapse
 
 
 Odd_dirs = ["/home/denis/samba_share/katrins_data/10147", # CT 2.0 PET 2.712871287128713
-"/home/denis/samba_share/katrins_data/10967",
+"/home/denis/samba_share/katrins_data/10967", # PET 1.4 
 "/home/denis/samba_share/katrins_data/11432", # PET 3.0 
 "/home/denis/samba_share/katrins_data/11749",
 "/home/denis/samba_share/katrins_data/13544",
@@ -73,6 +140,11 @@ Odd_dirs = ["/home/denis/samba_share/katrins_data/10147", # CT 2.0 PET 2.7128712
 
 
 x = 0
+
+ct_src = glob(Odd_dirs[x] + '/' + "*ct_from*")
+ct_src = ''.join(ct_src)
+
+"""
 
 ct_src = glob(Odd_dirs[x] + '/' + "*ct_from*")
 ct_src = ''.join(ct_src)
@@ -92,7 +164,7 @@ rel_src1 = glob(Odd_dirs[x] + '/' + "*Relapse*")
 rel_src1 = ''.join(rel_src1)
 rel_dst = Odd_dirs[x] + '/Cropped/Relapse.nii.gz'
 
-"""
+
 rel_src2 = glob(Odd_dirs[x] + '/' + "*relapse volume_R.nii*")
 rel_src2 = ''.join(rel_src2)
 
@@ -114,7 +186,7 @@ BinThreshImFilt.SetUpperThreshold(5)
 BinThreshImFilt.SetOutsideValue(0)
 BinThreshImFilt.SetInsideValue(1)
 BinIm = BinThreshImFilt.Execute(Im)
-"""
+
 
 CT = read_image(ct_src)
 CT_cropped = CT[:,:, 333:435]
@@ -143,7 +215,7 @@ sitk.WriteImage(CT_itk, ct_dst)
 shutil.copy2(pet_src, pet_dst)
 sitk.WriteImage(GTV_itk, gtv_dst)
 sitk.WriteImage(Relp_itk, rel_dst)
-
+"""
 
 
 ### Notes
