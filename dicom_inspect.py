44252cbe312beae5acb947415b5351c230fd8739
Denis Kutnár <deniskutnar@Deniss-MacBook-Pro.local>
2022-07-21 11:50:13 +0200

    “first”

--- dicom_inspect.py
+++ dicom_inspect.py
@@ -134,9 +134,9 @@ dicom2nifti.convert_directory(ct_dir, folder_out)
 #ct = convert_dcm_2_nii_x(ct_dir, folder_out)
 
 ### Remove Jason files 
-ct_js  = glob(folder_out + "*CT*.json")
-ct_js_rm = ''.join(ct_js)
-os.remove(ct_js_rm)
+#ct_js  = glob(folder_out + "*CT*.json")
+#ct_js_rm = ''.join(ct_js)
+#os.remove(ct_js_rm)
 pet_js = glob(folder_out + "*PET*.json")
 pet_js_rm = ''.join(pet_js)
 os.remove(pet_js_rm)
