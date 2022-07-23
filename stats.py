e8d794e40935eca197ac2a30ca02f697bff484cb
Denis Kutnár <deniskutnar@Deniss-MacBook-Pro.local>
2022-07-23 11:26:56 +0200

    “first”

--- stats.py
+++ stats.py
@@ -59,8 +59,14 @@ def read_image(path):
 
 
 ct_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/CT_CT*.nii.gz")
-print(ct_dirs)
+pet_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/PET_PET*.nii.gz")
+gtv_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/GTV*.nii.gz")
+relapse_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/Relapse*.nii.gz")
+
 print(len(ct_dirs))
+print(len(pet_dirs))
+print(len(gtv_dirs))
+print(len(relapse_dirs))
 
 
 exit()
