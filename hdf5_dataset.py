8243a6217df4046526cc2b2079a778ab35cf2dcd
Denis Kutnár <deniskutnar@Deniss-MacBook-Pro.local>
2022-07-24 09:21:30 +0200

    “first”

--- hdf5_dataset.py
+++ hdf5_dataset.py
@@ -13,6 +13,8 @@ import nibabel as nib
 import shutil
 import h5py
 
+import random
+
 
 ct_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/CT_CT*.nii.gz")
 pet_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/PET_PET*.nii.gz")
@@ -23,3 +25,9 @@ print(len(ct_dirs))
 print(len(pet_dirs))
 print(len(gtv_dirs))
 print(len(relapse_dirs))
+
+print(ct_dirs[0])
+
+### Shuffle dirs
+ct_dirs = random.shuffle(ct_dirs)
+print(ct_dirs[0])
