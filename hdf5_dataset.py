7c0f0fa6f0831721130cb62f9923d19924caef6c
Denis Kutnár <deniskutnar@Deniss-MacBook-Pro.local>
2022-07-24 09:29:43 +0200

    “first”

--- hdf5_dataset.py
+++ hdf5_dataset.py
@@ -17,17 +17,32 @@ import random
 
 
 ct_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/CT_CT*.nii.gz")
-pet_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/PET_PET*.nii.gz")
-gtv_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/GTV*.nii.gz")
-relapse_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/Relapse*.nii.gz")
-
-print(len(ct_dirs))
-print(len(pet_dirs))
-print(len(gtv_dirs))
-print(len(relapse_dirs))
-
-print(ct_dirs[0])
+#pet_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/PET_PET*.nii.gz")
+#gtv_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/GTV*.nii.gz")
+#relapse_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/Relapse*.nii.gz")
 
 ### Shuffle dirs
 random.shuffle(ct_dirs)
-print(ct_dirs[0])
+
+pet_dirs = []
+gtv_dirs = []
+relapse_dirs
+for idx,f in enumerate(ct_dirs):
+    base = os.path.basename(f).split('.nii.gz')[0]
+    
+    print(base)
+    pet = os.path.join(os.path.dirname(os.path.dirname(f)), 'imagesTr/'+ pid + '__PT.nii.gz')
+    #print(pet)
+
+
+    ct = os.path.join(os.path.dirname(os.path.dirname(f)), 'imagesTr/'+ pid + '__CT.nii.gz')
+    if os.path.exists(pet):
+        pet_dirs.append(pet)
+    if os.path.exists(ct):
+        ct_dirs.append(ct)
+
+
+
+
+### Split: 23 : 7 : 7
+
