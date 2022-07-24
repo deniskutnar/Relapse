01b3226d761834ba0dc25b6a34367e800f2c5897
Denis Kutnár <deniskutnar@Deniss-MacBook-Pro.local>
2022-07-24 09:15:08 +0200

    “first”

---
+++ hdf5_dataset.py
@@ -0,0 +1,25 @@
+import os
+import torch
+import torchvision
+import numpy as np
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import SimpleITK as sitk
+import glob
+
+import torch.nn as nn
+import nibabel as nib
+import shutil
+import h5py
+
+
+ct_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/CT_CT*.nii.gz")
+pet_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/PET_PET*.nii.gz")
+gtv_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/GTV*.nii.gz")
+relapse_dirs = glob("/home/denis/samba_share/katrins_data/*/Processed/Relapse*.nii.gz")
+
+print(len(ct_dirs))
+print(len(pet_dirs))
+print(len(gtv_dirs))
+print(len(relapse_dirs))
